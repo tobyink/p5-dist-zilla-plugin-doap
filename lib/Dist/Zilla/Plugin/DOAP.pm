7e96d4dd632617189c6960fbef77f53ed41a5b79
Toby Inkster <mail@tobyinkster.co.uk>
2014-03-14 13:39:23 +0000

    bump version; changelog

--- lib/Dist/Zilla/Plugin/DOAP.pm
+++ lib/Dist/Zilla/Plugin/DOAP.pm
@@ -5,7 +5,7 @@ package Dist::Zilla::Plugin::DOAP;
 # ABSTRACT: create a doap.xml file for your project
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.001';
+our $VERSION   = '0.002';
 
 use Moose;
 with qw(
